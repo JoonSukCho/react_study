29bc43d6bab895fe02161b11fc13649b69ef6283
chojsm <chojs28@gmail.com>
2020-12-04 09:20:08 +0900

    state 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,9 +5,10 @@ import IterationSample from "./IterationSample";
 import LifeCycleSample from "./LifeCycleSample";
 import Info from "./Info";
 import Average from "./Average";
+import Practice from "./Practice";
 import "./App.css";
 
 const App = () => {
-  return <Average></Average>;
+  return <Practice></Practice>;
 };
 export default App;
---
+++ hello-react/src/Practice.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Practice = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <h1>x</h1>
+    </div>
+  );
+};
+
+export default Practice;
